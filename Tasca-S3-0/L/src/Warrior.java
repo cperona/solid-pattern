9bf7d4f44bb7cdeb72f2c344c69c2b5b1e812e67
“Christian <113365748+cperona@users.noreply.github.com>
2026-04-22 13:36:27 +0200

    fix: remove takeDamage(int points) override to align better with Liskov Substitution priciple

--- Tasca-S3-0/L/src/Warrior.java
+++ Tasca-S3-0/L/src/Warrior.java
@@ -7,9 +7,4 @@ public class Warrior extends Character {
     public void attack() {
         System.out.println("The warrior strikes with a sword.");
     }
-
-    @Override
-    public void takeDamage(int points) {
-        System.out.println("The warrior resists and only takes " + (points / 2) + " points of damage.");
-    }
 }
